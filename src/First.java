fea21e7dc5a0c37f11a8cb7d97b758cc1d430c45
SabaMicheal <57517096+SabaMicheal@users.noreply.github.com>
2019-11-09 16:30:38 -0600

    set up

---
+++ src/First.java
@@ -0,0 +1,12 @@
+public class First {
+    public static void main(String[]args){
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hi");
+        System.out.println("bay");
+       // add method
+        //if condition
+
+    }
+}
+
