9dd62bc965213a2634eca50e86e35602a224ece9
SabaMicheal <57517096+SabaMicheal@users.noreply.github.com>
2019-12-10 21:02:45 -0600

    first our project

---
+++ src/staticClass/Stuentskybertake.java
@@ -0,0 +1,17 @@
+package staticClass;
+
+import SwitchStatements.Rondom;
+
+import java.util.Random;
+
+public class Stuentskybertake {
+    public  double pI = 3.14;
+    static  double zero = 0;
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println(random.nextInt(50));
+
+    }
+        }
+
