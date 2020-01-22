bf343da3f9747732e9149e1a230614814f9adffe
SabaMicheal <57517096+SabaMicheal@users.noreply.github.com>
2020-01-21 22:21:54 -0600

    first our project

---
+++ src/acadamy/learnprgramming/MYNAME.java
@@ -0,0 +1,16 @@
+package acadamy.learnprgramming;
+
+public class MYNAME {
+    public static void main(String[]args){
+
+        String Name = "Sera";
+        int Grade = 12 + 1;
+        String Nationality = "Eritrea";
+        int Age = 24;
+        String MarriageStatus = "Single";
+
+        System.out.println(Name  + " " + Age + " " +  " " + Nationality + " " + Grade + " " + MarriageStatus);
+
+
+    }
+}
---
+++ src/acadamy/learnprgramming/Pizza.java
@@ -0,0 +1,21 @@
+package acadamy.learnprgramming;
+
+public class Pizza {
+    public static void main(String[] args) {
+        String str =  new String("I love hamburger.");
+        String jason = new String("And I also love hot dogs.");
+
+        String str2 = "I like to go to Disneyland.";
+        String gibson = "45878 is my favorite number.";
+
+        System.out.println(str);
+        System.out.println(jason);
+        System.out.println();
+        System.out.println(str2);
+        System.out.println(gibson);
+        System.out.println();
+
+        System.out.println("Valentine's Day " + str);
+        System.out.println(str + str2);
+    }
+}
